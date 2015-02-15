cb0f9eb9055aaf13a9a56f612c62e92964bba678
baffolobill <baffolobill@yandex.ru>
2015-02-15 23:47:01 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(name='extras_mongoengine',
       version='0.1',
       description='MongoEngine Extras - Field Types and any other wizardry.',
-      url='https://github.com/MongoEngine/extras-mongoengine/',
+      url='https://github.com/baffolobill/extras-mongoengine/',
       install_requires=['mongoengine>=0.8.6'],
       packages=['extras_mongoengine'],
      )
